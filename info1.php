9c07079c2e9bd0e76a7ad3179675bd3faa7f70ea
Santos <lui_5600@hotmail.com>
2013-10-04 13:12:52 -0700

    Video
    Ion-Fusion video

---
+++ info1.php
@@ -0,0 +1,51 @@
+<!-- Home Left Column Begins -->
+<div id="HomeColumn1">
+	<div id="flashvideo2">
+		<object width="640" height="350" >
+			<param name="movie" value="videoplayer.swf" />
+			<param name="quality" value="best" />
+			<param name="bgcolor" value="#000000" />
+			<param name="play" value="true" />
+			<param name="loop" value="true" />
+			<param name="wmode" value="transparent" />
+			<param name="scale" value="tofit" />
+			<param name="allowFullScreen" value="true" />
+			<param name="flashvars" value="autoplay=true&amp;v=media/NEUROMAMA_ION_FUSION_COMPLETE_VIDEO.flv&amp;img=Untitled-6.png&amp;repeat=true&amp;btnIcon=#6CFF00&amp;playIcon=#6CFF00&amp;controlsBgAlpha=0.9&amp;bgGradient1=#000000&amp;bgGradient2=#101010" />
+			<param name="menu" value="true" />
+			<param name="devicefont" value="false" />
+			<param name="salign" value="" />
+			<param name="allowScriptAccess" value="always" />
+			<object id="player" type="application/x-shockwave-flash" data="videoplayer.swf" width="450" height="300">
+				<param name="movie" value="videoplayer.swf" />
+				<param name="quality" value="best" />
+				<param name="bgcolor" value="#000000" />
+				<param name="play" value="true" />
+				<param name="loop" value="true" />
+				<param name="allowFullScreen" value="true" />
+				<param name="flashvars" value="autoplay=true&amp;v=media/NEUROMAMA_ION_FUSION_COMPLETE_VIDEO.flv&amp;img=Untitled-6.png&amp;repeat=true&amp;btnIcon=#6CFF00&amp;playIcon=#6CFF00&amp;controlsBgAlpha=0.9&amp;bgGradient1=#000000&amp;bgGradient2=#101010" />
+				<param name="wmode" value="transparent" />
+				<param name="scale" value="tofit" />
+				<param name="menu" value="true" />
+				<param name="devicefont" value="false" />
+				<param name="salign" value="" />
+				<param name="allowScriptAccess" value="always" />
+			</object>
+		</object>
+		<br/>
+		<span class="style2" style="display: block; margin: 0px auto; text-align: center;">
+		</span>
+	</div>
+	<div id="flashvideo2holder"></div>	
+	<div id="HomeGalleryDetailsContainer">
+		<!-- Item 1 -->	
+		<div class="newsdiv">
+			<!--a class="eurasiapdf" href="#" onclick="TINY.box.show({url:'viewerpdf.html',width:1028,height:450})"><img  style="border:0px"src="images2/eurasiapdf.png" height="100%" width="80%"/></a-->
+			<!--a class="eurasiapdf" href="viewerpdf.html" target="_blank"><img  src="images2/eurasiapdf.png" height="100%" width="80%"/></a-->
+		</div>
+	</div><!-- Home Left Column Ends -->
+
+		<!-- Home Right Column Begins -->
+</div>
+
+<!-- Home Right Column Ends -->
+<div id="SpacerDiv" style="height: 2px; width: 710px;"></div>
